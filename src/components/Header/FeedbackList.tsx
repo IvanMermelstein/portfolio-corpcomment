f264610e362bc3e8406c13ea47fdcf54544917ca
Iván Mermelstein <ivan.mermelstein@gmail.com>
2024-04-21 20:30:51 -0300

    add feedbacklist

--- src/components/Header/FeedbackList.tsx
+++ src/components/Header/FeedbackList.tsx
@@ -1,5 +1,27 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 const FeedbackList = () => {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+        <div>
+          <p>I</p>
+        </div>
+        <div>
+          <p>Ivan</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta alias
+            veniam quos consectetur illum ad.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 };
 
 export default FeedbackList;
